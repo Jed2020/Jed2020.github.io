3b01e0da502286b99900042de3d43b0c2b5d935b
emerson <69053988+Jed2020@users.noreply.github.com>
2021-10-15 10:03:58 -0300

    comentario no js

--- navbar.js
+++ navbar.js
@@ -1,8 +1,10 @@
 const btnMobile = document.getElementById('btn-mobile');
-
+//função verifica o tipo de evento
 function toggleMenu(event) {
+  //se o evento for tipo touchstart, esse event enibe a sequência, ou seja enibe o click
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
+  //toggle adiciona caso tenha e remova caso não tenha
   nav.classList.toggle('active');
   const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
